dcd4451182ce1311b220774b9a1ff794bf78abc4
yangqingmang <tiandibawang@126.com>
2019-04-13 11:42:11 +0800

    增加client

--- spring-cloud-eureka-server/src/main/java/com/yangqingmang/springcloud/eureka/server/EurekaServerApplication.java
+++ spring-cloud-eureka-server/src/main/java/com/yangqingmang/springcloud/eureka/server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.yangqingmang.cloud.eureka.server;
+package com.yangqingmang.springcloud.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
